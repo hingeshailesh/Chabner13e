16d55040f55eddf0f7f5a898bd099ec66bb8bcc9
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2025-08-08 16:53:47 +0530

    Downloaded from SFTP

---
+++ input_08Aug2025/9780443110122/OEBPS/question_widgets/MCSS/js/ch11_2_ass.js
@@ -0,0 +1,58 @@
+//json_file_format
+var quiz = [
+
+    {
+        q: `<b>Case 2:</b> Mr. Smith is brought urgently to the Emergency Room for prolonged chest pain. His ECG showed ST segment elevation in the anterior leads and he was then transferred to the cardiac cath lab. An angiogram reveals 100% blockage of the left anterior descending (LAD) coronary artery. PCI with stent is recommended.`,
+        q2: `What did the ECG reveal?`,
+        q3: [],
+        option: [
+`STEMI`,
+`NSTEMI and unstable angina`,
+`Aortic aneurysm`,
+`CHF`],
+        optionFeedback: [],
+        optionStyleType: `none`,
+        answer: 0,
+        ansText: ``,
+        state: `notAnswered`,
+        userAnswered: ``
+    },
+
+
+    {
+        q: `Your diagnosis for this patient is:`,
+       
+        q3: [],
+        option: [
+`Patent ductus arteriosus`,
+`Unstable angina`,
+`Heart attack`,
+`Rheumatic heart disease`],
+        optionFeedback: [],
+        optionStyleType: `none`,
+        answer: 2,
+        ansText: ``,
+        state: `notAnswered`,
+        userAnswered: ``
+    },
+
+
+    {
+        q: `What treatment is recommended?`,
+       
+        q3: [],
+        option: [
+`Deﬁbrillation and cardioversion`,
+`Thrombolytic drugs`,
+`Catheterization with drug-eluting stent placement`,
+`Coronary artery bypass grafting`],
+        optionFeedback: [],
+        optionStyleType: `none`,
+        answer: 2,
+        ansText: ``,
+        state: `notAnswered`,
+        userAnswered: ``
+    }
+ 
+
+];
