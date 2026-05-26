528e8ecc406effc754c6886721422522337bce75
parag-saple_mcgraw <89586488+motpuse-naveen@users.noreply.github.com>
2026-05-26 10:24:13 +0530

    Latest - 9780443110139 - epub received from ganesh and rohan.

---
+++ 9780443110139/OEBPS/question_widgets/MCSS/js/ch11_1_ass.js
@@ -0,0 +1,22 @@
+//json_file_format
+var quiz = [
+
+    {
+        q: `<b>Case 1:</b>  A 24-year-old woman with a history of palpitations [heartbeat is unusually strong, rapid, or irregular, so that patient is aware of it] and vague chest pains enters the hospital. With the patient supine, you hear a midsystolic click that is followed by a grade 3/6 [moderately loud—6/6 is loud and 1/6 is quiet] honking murmur.`,
+        q2: `Your diagnosis is:`,
+        q3: [],
+        option: [
+`Congestive heart failure`,
+`Mitral valve prolapse`,
+`Tetralogy of Fallot`,
+`Raynaud disease`],
+        optionFeedback: [],
+        optionStyleType: `none`,
+        answer: 1,
+        ansText: ``,
+        state: `notAnswered`,
+        userAnswered: ``
+    },
+ 
+
+];
---
+++ 9780443110139/OEBPS/question_widgets/MCSS/js/ch11_3_ass.js
@@ -0,0 +1,39 @@
+//json_file_format
+var quiz = [
+
+    {
+        q: `<b>Case 3:</b> A 42-year-old female runner recovering from an upper respiratory infection comes to the ED complaining of chest pain that is sharp and constant, worse when she is lying down and decreased with sitting up and leaning forward. Serum troponin levels rule out an acute MI. The ED physician auscultates a pericardial friction rub.`,
+        q2: `What’s your diagnosis for this patient?`,
+        q3: [],
+        option: [
+`Pericarditis`,
+`Myocardial ischemia`,
+`Unstable angina and NSTEMI`,
+`Endocarditis`],
+        optionFeedback: [],
+        optionStyleType: `none`,
+        answer: 0,
+        ansText: ``,
+        state: `notAnswered`,
+        userAnswered: ``
+    },
+
+     {
+        q: `The danger of this condition is the risk for progression to:`,
+       
+        q3: [],
+        option: [
+`Claudication`,
+`Pulmonary embolism`,
+`Cardiac tamponade`,
+`Aneurysm`],
+        optionFeedback: [],
+        optionStyleType: `none`,
+        answer: 2,
+        ansText: ``,
+        state: `notAnswered`,
+        userAnswered: ``
+    },
+ 
+
+];
